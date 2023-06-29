d0d0206deb5b1db9808d17bb330181f77a952bb2
Janaki <zanuthulung@gmail.com>
2023-06-29 14:36:23 +0545

    feat: design myprofile

--- src/component/MyProfile.jsx
+++ src/component/MyProfile.jsx
@@ -1,35 +1,71 @@
 import React from "react";
-import { Box, Container, CssBaseline, Grid, Typography } from "@mui/material";
+import { Box, Button, Container, CssBaseline, Grid, Typography } from "@mui/material";
 import { useGetMyProfileQuery } from "../services/api/admin/auth";
-
+import { display } from "@mui/system";
+import PersonIcon from '@mui/icons-material/Person';
+import EmailIcon from '@mui/icons-material/Email';
+import PhoneIcon from '@mui/icons-material/Phone';
+import MaleIcon from '@mui/icons-material/Male';
+import GroupsIcon from '@mui/icons-material/Groups';
+import FemaleIcon from '@mui/icons-material/Female';
+import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
 const MyProfile = () => {
   const { data } = useGetMyProfileQuery();
 
+  const getGenderIcon = () => {
+    if (data && data.data.gender === 'female') {
+      return <FemaleIcon sx={{ position: 'relative', bottom: '2px' }} />
+    } else {
+      return <MaleIcon sx={{ position: 'relative', bottom: '2px' }} />;
+    }
+  }
+
   return (
-    <Container component="main" maxWidth="xs">
+    <Container component="main" >
       <CssBaseline />
       <Box
         sx={{
-          marginTop: 4,
+          marginTop: 1,
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
           marginBottom: 8,
         }}
       >
-        <img
-          src="smile.png"
-          alt="Profile Picture"
-          style={{
-            height: "80px",
-            width: "80px",
-            marginRight: "16px",
-            borderRadius: "100%",
-          }}
-        />
+        <Container style={{
+          backgroundColor: 'rgb(231, 229, 229)',
+          width: '80%',
+          borderTopLeftRadius: '50px'
+        }} >
+          <img
+            src="smile.png"
+            alt="Profile Picture"
+            style={{
+              height: "80px",
+              width: "80px",
+              marginRight: "16px",
+              borderRadius: "100%",
+              marginTop: "110px",
+              position: 'relative',
+              top: '65px'
+            }}
+          />
+        </Container>
 
-        <Typography component="h1" variant="h5" mt={2}>
+        <Typography component="h1" variant="h5" mt={1} style={{
+          marginInlineEnd: '46%',
+          textAlign: 'left',
+
+        }}>
           Myprofile
+          <Button variant="contained" sx={{
+            position: 'absolute',
+            right: '150px',
+            top: '305px',
+            display: 'flex',
+            gap: '7px',
+
+          }}><EditOutlinedIcon />Edit Profile</Button>
         </Typography>
         <Box
           sx={{
@@ -42,26 +78,37 @@ const MyProfile = () => {
           }}
         >
           {data && (
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <Typography>
+            <Grid container spacing={2} style={{
+              marginRight: '50px',
+              height: '160px',
+              textAlign: 'left',
+
+            }}>
+
+              <Grid item xs={12}   >
+                <Typography sx={{ display: 'flex', gap: '10px', flexDirection: 'row' }}><PersonIcon sx={{ position: 'relative', bottom: '2px' }} />
                   {data.data.firstName} {data.data.lastName}
                 </Typography>
               </Grid>
 
-              <Grid item xs={12}>
-                <Typography>{data.data.email}</Typography>
+              <Grid item xs={12} >
+                <Typography sx={{ display: 'flex', gap: '10px', flexDirection: 'row' }}>< EmailIcon sx={{ position: 'relative', bottom: '2px' }} />
+                  {data.data.email}</Typography>
               </Grid>
               <Grid item xs={12}>
-                <Typography>{data.data.phoneNumber}</Typography>
+                <Typography sx={{ display: 'flex', gap: '10px', flexDirection: 'row' }}><PhoneIcon sx={{ position: 'relative', bottom: '2px' }} />
+                  {data.data.phoneNumber}</Typography>
+              </Grid>
+              <Grid item xs={12} >
+                <Typography sx={{ display: 'flex', gap: '10px', flexDirection: 'row' }}> {getGenderIcon()}
+                  {data.data.gender.charAt(0).toUpperCase() + data.data.gender.slice(1)}</Typography>
               </Grid>
               <Grid item xs={12}>
-                <Typography>{data.data.gender}</Typography>
-              </Grid>
-              <Grid item xs={12}>
-                <Typography>{data.data.roles}</Typography>
+                <Typography sx={{ display: 'flex', gap: '10px', flexDirection: 'row' }}><GroupsIcon x={{ position: 'relative', bottom: '2px' }} />
+                  {data.data.roles.map((role) => role.toLowerCase() === 'admin' ? role.charAt(0).toUpperCase() + role.slice(1) : role)}</Typography>
               </Grid>
             </Grid>
+
           )}
         </Box>
       </Box>
