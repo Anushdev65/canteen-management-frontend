267884256e847edff9589f22604deff414df0b88
Janaki <zanuthulung@gmail.com>
2023-07-06 13:51:05 +0545

    feat:updated login design

--- src/pages/LoginForm.jsx
+++ src/pages/LoginForm.jsx
@@ -17,6 +17,7 @@ import MUIToast from "../component/MUIToast";
 import { setLevelInfo, setUSerInfo } from "../localStorage/localStorage";
 import { useNavigate } from "react-router-dom";
 import MUILoading from "../component/MUILoading";
+import "./login.css";
 
 const initialValues = {
   password: "",
@@ -51,107 +52,121 @@ export default function LoginForm() {
 
   return (
     <>
-      {data ? (
-        <MUIToast
-          initialValue={true}
-          message={data.message}
-          severity="success"
-        />
-      ) : error ? (
-        <MUIToast
-          initialValue={true}
-          message={error.data.message}
-          severity="error"
-        />
-      ) : (
-        <></>
-      )}
-      {isLoading || data ? (
-        <MUILoading />
-      ) : (
-        <Container component="main" maxWidth="xs">
-          <CssBaseline />
-          <Box
+      <div className="login-container">
+        {data ? (
+          <MUIToast
+            initialValue={true}
+            message={data.message}
+            severity="success"
+          />
+        ) : error ? (
+          <MUIToast
+            initialValue={true}
+            message={error.data.message}
+            severity="error"
+          />
+        ) : (
+          <></>
+        )}
+        {isLoading || data ? (
+          <MUILoading />
+        ) : (
+          <Container
+            component="main"
+            maxWidth="xs"
             sx={{
-              marginTop: 4,
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              marginBottom: 8,
+              height: "475px",
+              paddingTop: "5px",
+              borderRadius: "20px",
+              opacity: 1.21,
+              boxShadow: "7px 7px 6px 0 rgba(0, 0, 0, 0.4)",
+              transition: "box-shadow 0.3",
+              "&:hover": { boxShadow: "12px 12px 6px 0 rgba(0, 0, 0, 0.4)" },
             }}
           >
-            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Login
-            </Typography>
+            <CssBaseline />
             <Box
-              component="form"
-              noValidate
-              onSubmit={handleSubmit}
-              sx={{ mt: 3 }}
+              sx={{
+                marginTop: 4,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                marginBottom: 8,
+              }}
             >
-              <Grid container spacing={2}>
-                <Grid item xs={12}>
-                  <TextField
-                    error={touched.email && errors.email}
-                    required
-                    fullWidth
-                    id="email"
-                    label="Email Address"
-                    name="email"
-                    autoComplete="off"
-                    value={values.email}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                  />
-                  <MUIError
-                    touch={touched.email}
-                    error={errors.email}
-                    value={false}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
-                    error={touched.password && errors.password}
-                    required
-                    fullWidth
-                    name="password"
-                    label="Password"
-                    type="password"
-                    id="password"
-                    autoComplete="new-password"
-                    value={values.password}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                  />
-                  <MUIError
-                    touch={touched.password}
-                    error={errors.password}
-                    value={false}
-                  />
-                </Grid>
-              </Grid>
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
+              <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+                <LockOutlinedIcon />
+              </Avatar>
+              <Typography component="h1" variant="h5">
                 Login
-              </Button>
-              <Grid container justifyContent="flex-end" spacing={1}>
-                <Grid item>
-                  <Link href="/forgot-password" variant="body2">
-                    Forgot Password?
-                  </Link>
+              </Typography>
+              <Box
+                component="form"
+                noValidate
+                onSubmit={handleSubmit}
+                sx={{ mt: 3 }}
+              >
+                <Grid container spacing={2}>
+                  <Grid item xs={12} sx={{ marginTop: "20px" }}>
+                    <TextField
+                      error={touched.email && errors.email}
+                      required
+                      fullWidth
+                      id="email"
+                      label="Email Address"
+                      name="email"
+                      autoComplete="off"
+                      value={values.email}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
+                    <MUIError
+                      touch={touched.email}
+                      error={errors.email}
+                      value={false}
+                    />
+                  </Grid>
+                  <Grid item xs={12} sx={{ marginTop: "10px" }}>
+                    <TextField
+                      error={touched.password && errors.password}
+                      required
+                      fullWidth
+                      name="password"
+                      label="Password"
+                      type="password"
+                      id="password"
+                      autoComplete="new-password"
+                      value={values.password}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
+                    <MUIError
+                      touch={touched.password}
+                      error={errors.password}
+                      value={false}
+                    />
+                  </Grid>
                 </Grid>
-              </Grid>
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ marginTop: "20px" }}
+                >
+                  Login
+                </Button>
+                <Grid container justifyContent="flex-end" spacing={1}>
+                  <Grid item sx={{ marginTop: "20px" }}>
+                    <Link href="/forgot-password" variant="body2">
+                      Forgot Password?
+                    </Link>
+                  </Grid>
+                </Grid>
+              </Box>
             </Box>
-          </Box>
-        </Container>
-      )}
+          </Container>
+        )}
+      </div>
     </>
   );
 }
