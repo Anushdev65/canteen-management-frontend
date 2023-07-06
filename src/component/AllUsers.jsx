015b55edfc56e512fecc43fc4f7d1c9367586803
y-ks <66983245+y-ks@users.noreply.github.com>
2023-07-06 13:11:32 +0545

    feat: add table pagination

--- src/component/AllUsers.jsx
+++ src/component/AllUsers.jsx
@@ -1,48 +1,50 @@
-import React, { useEffect, useState, useMemo } from "react";
-import "./table.css";
+import React, { useEffect, useMemo, useState } from "react";
+import { usePagination, useSortBy, useTable } from "react-table";
+import { useLazyGetAllUsersQuery } from "../services/api/admin/auth";
 import { COLUMNS } from "./columns";
-import { useTable, useSortBy, usePagination } from "react-table";
-import {
-  useGetAllUsersQuery,
-  useLazyGetAllUsersQuery,
-} from "../services/api/admin/auth";
-// import { TableContainer, Table, TableHead, TableBody, TableRow, TableCell, Paper, Checkbox } from '@mui/material'
+import "./table.css";
 
 const AllUsers = () => {
   const columns = useMemo(() => COLUMNS, []);
   const [trigger, { data }] = useLazyGetAllUsersQuery();
-  // const { data } = useGetAllUsersQuery();
-  const tableData = useMemo(() => data?.data?.results || [], [data]);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const tableData = useMemo(
+    () => data?.data?.results || [],
+    [data?.data?.results]
+  );
 
   useEffect(() => {
-    trigger({ _page: 1 });
-  }, []);
+    trigger({ _page: currentPage, _brake: rowsPerPage });
+  }, [currentPage, rowsPerPage]);
 
   useEffect(() => {
     console.log(data);
   }, [data]);
 
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    page,
-    nextPage,
-    previousPage,
-    prepareRow,
-  } = useTable(
-    {
-      columns,
-      data: tableData,
-    },
-    useSortBy,
-    usePagination
-  );
+  const { getTableProps, getTableBodyProps, headerGroups, page, prepareRow } =
+    useTable(
+      {
+        columns,
+        data: tableData,
+        initialState: { pageSize: rowsPerPage },
+      },
+      useSortBy,
+      usePagination
+    );
 
-  // useEffect(() => {
-  //   console.log("data test");
-  //   console.log(data);
-  // }, [data]);
+  const handleRowSelect = (e) => {
+    const selectedRowsPerPage = e.target.value;
+    setRowsPerPage(selectedRowsPerPage);
+    setCurrentPage(1);
+    trigger({ _page: 1, _brake: selectedRowsPerPage });
+  };
+
+  const handlePage = (pageNumber) => {
+    setCurrentPage(pageNumber);
+    trigger({ _page: pageNumber, _brake: rowsPerPage });
+  };
 
   return (
     <>
@@ -80,9 +82,54 @@ const AllUsers = () => {
           })}
         </tbody>
       </table>
-      <div>
-        <button onClick={() => trigger({ _page: 1 })}>Previous</button>
-        <button onClick={() => trigger({ _page: 2 })}> Next</button>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <span>
+          Rows per page:{" "}
+          <select onChange={handleRowSelect} value={rowsPerPage}>
+            {[5, 10, 15, 20].map((value, index) => (
+              <option key={index} value={value}>
+                {value}
+              </option>
+            ))}
+          </select>
+        </span>
+
+        <span>
+          Page{" "}
+          <strong>
+            {data?.data.currentPage} of {data?.data?.totalPage}
+          </strong>
+        </span>
+        <button
+          disabled={!data?.data?.hasPreviousPage}
+          onClick={() => handlePage(1)}
+        >
+          {"<<"}
+        </button>
+        <button
+          onClick={() => handlePage(data?.data?.currentPage - 1)}
+          disabled={!data?.data?.hasPreviousPage}
+        >
+          Previous
+        </button>
+        <button
+          onClick={() => handlePage(data?.data?.currentPage + 1)}
+          disabled={!data?.data?.hasNextPage}
+        >
+          {" "}
+          Next
+        </button>
+        <button
+          disabled={!data?.data?.hasNextPage}
+          onClick={() => handlePage(data?.data?.totalPage)}
+        >
+          {">>"}
+        </button>
       </div>
     </>
   );
