67c950fc8d848c91da520d3d6029e82b51936cad
y-ks <66983245+y-ks@users.noreply.github.com>
2023-07-22 15:55:25 +0545

    fix: addQuantity

--- src/component/landingContent/LandingContent.jsx
+++ src/component/landingContent/LandingContent.jsx
@@ -6,7 +6,7 @@ const LandingContent = () => {
     <div className={styles.animationContainer}>
       <div className={styles.container}>
         <div className={styles.floatingText}>
-          <img src="logodeerwalk.png"></img>Welcome to Deerwalk Foods
+          <img src="logodeerwalk.png" alt=""></img>Welcome to Deerwalk Foods
         </div>
       </div>
     </div>
