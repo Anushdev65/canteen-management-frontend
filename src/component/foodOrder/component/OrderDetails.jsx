fd1039476635c3ea37c69ffc32a5d99b3f4ebd38
Janaki <zanuthulung@gmail.com>
2023-07-24 15:08:38 +0545

    feat:updated ordertable

--- src/component/foodOrder/component/OrderDetails.jsx
+++ src/component/foodOrder/component/OrderDetails.jsx
@@ -13,7 +13,7 @@ import {
   useServeOrderMutation,
 } from "../../../services/api/foodOrder";
 import MUIToast from "../../MUIToast";
-
+import "../../../menuorder/myorder.css";
 const OrderDetails = () => {
   const { data } = useGetMyOrdersQuery();
   const [cancelOrder, { data: cancelData, error: cancelError }] =
@@ -47,94 +47,97 @@ const OrderDetails = () => {
 
   return (
     <>
-      {myOrder?.length > 0 && (
-        <div style={{ display: "flex", justifyContent: "center" }}>
-          <div>
-            <h2>Order Details:</h2>
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell>Item</TableCell>
-                  <TableCell>Quantity</TableCell>
-                  <TableCell>Price</TableCell>
-                  <TableCell>Action</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {myOrder.map((item, index) => (
-                  <TableRow key={index}>
-                    <TableCell>{item.food.name}</TableCell>
-                    <TableCell>{item.quantity}</TableCell>
-                    <TableCell>{item.food.rate}</TableCell>
-                    <TableCell>
-                      <Button
-                        variant="contained"
-                        color="secondary"
-                        size="small"
-                        disabled={item.orderStatus !== "onProcess"}
-                        onClick={() => handleCancelOrder(item._id)}
-                      >
-                        Cancel
-                      </Button>
-                    </TableCell>
+      <div className="myorder">
+        {myOrder?.length > 0 && (
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <div>
+              <h2>Order Details:</h2>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Item</TableCell>
+                    <TableCell>Quantity</TableCell>
+                    <TableCell>Price</TableCell>
+                    <TableCell>Action</TableCell>
                   </TableRow>
-                ))}
-                <TableRow>
-                  <TableCell colSpan={2} align="right">
-                    Total:
-                  </TableCell>
-                  <TableCell>
-                    {myOrder.reduce(
-                      (total, item) => total + item.food.rate * item.quantity,
-                      0
-                    )}
-                  </TableCell>
-                  <TableCell></TableCell>
-                </TableRow>
-              </TableBody>
-            </Table>
+                </TableHead>
+                <TableBody>
+                  {myOrder.map((item, index) => (
+                    <TableRow key={index}>
+                      <TableCell>{item.food.name}</TableCell>
+                      <TableCell>{item.quantity}</TableCell>
+                      <TableCell>{item.food.rate}</TableCell>
+                      <TableCell>
+                        <Button
+                          variant="contained"
+                          color="secondary"
+                          size="small"
+                          disabled={item.orderStatus !== "onProcess"}
+                          onClick={() => handleCancelOrder(item._id)}
+                        >
+                          Cancel
+                        </Button>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                  <TableRow>
+                    <TableCell colSpan={2} align="right">
+                      Total:
+                    </TableCell>
+                    <TableCell>
+                      {myOrder.reduce(
+                        (total, item) => total + item.food.rate * item.quantity,
+                        0
+                      )}
+                    </TableCell>
+                    <TableCell></TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
 
-            <Button
-              variant="contained"
-              color="primary"
-              size="small"
-              sx={{ mt: 2, ml: 2 }}
-              onClick={handleServeOrder}
-              disabled={!onProcessOrder}
-            >
-              {onProcessOrder ? "Serve Me" : "Served"}
-            </Button>
+              <Button
+                id="order-servebtn"
+                variant="contained"
+                color="primary"
+                size="small"
+                sx={{ mt: 2, ml: 2 }}
+                onClick={handleServeOrder}
+                disabled={!onProcessOrder}
+              >
+                {onProcessOrder ? "Serve Me" : "Served"}
+              </Button>
+            </div>
           </div>
-        </div>
-      )}
-      {serveData && (
-        <MUIToast
-          initialValue={true}
-          message={serveData.message}
-          severity="success"
-        />
-      )}
-      {serveError && (
-        <MUIToast
-          initialValue={true}
-          message={serveError.data.message}
-          severity="error"
-        />
-      )}
-      {cancelData && (
-        <MUIToast
-          initialValue={true}
-          message={cancelData.message}
-          severity="success"
-        />
-      )}
-      {cancelError && (
-        <MUIToast
-          initialValue={true}
-          message={cancelError.data.message}
-          severity="error"
-        />
-      )}
+        )}
+        {serveData && (
+          <MUIToast
+            initialValue={true}
+            message={serveData.message}
+            severity="success"
+          />
+        )}
+        {serveError && (
+          <MUIToast
+            initialValue={true}
+            message={serveError.data.message}
+            severity="error"
+          />
+        )}
+        {cancelData && (
+          <MUIToast
+            initialValue={true}
+            message={cancelData.message}
+            severity="success"
+          />
+        )}
+        {cancelError && (
+          <MUIToast
+            initialValue={true}
+            message={cancelError.data.message}
+            severity="error"
+          />
+        )}
+      </div>
     </>
   );
 };
