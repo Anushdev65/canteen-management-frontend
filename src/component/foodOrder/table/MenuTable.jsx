82e4d16140ad0e18dc34a3c13fd9719625869c63
Rashmi-KC <rashmikc80@gmail.com>
2023-07-25 09:08:14 +0545

    feat: added available balance in navbar

--- src/component/foodOrder/table/MenuTable.jsx
+++ src/component/foodOrder/table/MenuTable.jsx
@@ -135,7 +135,11 @@ const MenuTable = () => {
       table.createDataColumn("availableTime.from", {
         id: "Available Time",
         cell: (props) => {
-          if (!props.row.getCanExpand() && !props.row.originalSubRows)
+          if (
+            !props.row.getCanExpand() &&
+            !props.row.originalSubRows &&
+            props.row.original.availableTime
+          ) {
             return (
               <div>
                 {dayjs(props.row.original.availableTime.from).format("HH:mm A")}
@@ -143,6 +147,9 @@ const MenuTable = () => {
                 {dayjs(props.row.original.availableTime.to).format("HH:mm A")}
               </div>
             );
+          } else {
+            return null;
+          }
         },
       }),
 
